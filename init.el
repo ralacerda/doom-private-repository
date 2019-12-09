6a0515a753cf18afb7e0e4482ecd968e8f329b8e
Renato Lacerda <renato.ac.lacerda@gmail.com>
2019-12-09 16:30:12 -0300

    Added magit module

--- init.el
+++ init.el
@@ -84,7 +84,7 @@
        ;; +docsets)        ; ...or in Dash docsets locally
        ;;lsp
        ;;macos             ; MacOS-specific commands
-       ;;magit             ; a git porcelain for Emacs
+       magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
