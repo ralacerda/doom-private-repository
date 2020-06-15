708e2f02def986d7d3c724e61ef66b003c3cb90d
Renato Lacerda <renato.ac.lacerda@gmail.com>
2020-06-14 21:03:09 -0300

    Reseting my config

--- packages.el
+++ packages.el
@@ -1,10 +1,1 @@
-;; -*- no-byte-compile: t; -*-
-;;; .config/doom/packages.el
-
-;;; Examples:
-;; (package! some-package)
-;; (package! another-package :recipe (:host github :repo "username/repo"))
-;; (package! builtin-package :disable t)
-
-(package! csv-mode)
-(package! polymode)
+ 
